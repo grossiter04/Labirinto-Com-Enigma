53ad76d9649734daf17a9e192543f1e12815b8f2
Gabriel Rossiter <gabrielrossiter04@hotmail.com>
2023-11-08 10:11:17 -0300

    Mudando o jogo para o labirinto

--- main.c
+++ main.c
@@ -1,69 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void telaInicial();
-void limpaTela();
+// Definimos a estrutura da sala
+typedef struct Sala {
+    int numero_sala;
+    char descricao[100];
+    struct Sala* esquerda;
+    struct Sala* direita;
+} Sala;
 
+// Função para criar uma sala
+void criarSala(Sala** sala, int numero_sala, const char descricao[]) {
+    *sala = (Sala*)malloc(sizeof(Sala));
+    if (*sala == NULL) {
+        printf("Erro na alocação de memória\n");
+        exit(1);
+    }
+    (*sala)->numero_sala = numero_sala;
+    strcpy((*sala)->descricao, descricao);
+    (*sala)->esquerda = NULL;
+    (*sala)->direita = NULL;
+}
 
-int main(){
+// Função para conectar salas
+void conectarSalas(Sala* de, Sala* sala_esquerda, Sala* sala_direita) {
+    de->esquerda = sala_esquerda;
+    de->direita = sala_direita;
+}
+
+int main() {
+    //aqui vai iniciar as salas
+    Sala* sala_inicial = NULL;
+    Sala* sala_esquerda = NULL;
+    Sala* sala_direita = NULL;
+    Sala* sala_20 = NULL;
+    Sala* sala_8 = NULL;
+
+    //aqui voce vai colocar as caracteristicas da sala  (&nomeDaSala, numero da sala, Comentario da sala);
+    criarSala(&sala_inicial, 10, "Você está no começo do labirinto. Escolha para onde deseja ir.");
+    criarSala(&sala_esquerda, 9, "Você está na sala à esquerda. Há uma porta à direita.");
+    criarSala(&sala_direita, 11, "Você está na sala à direita. Há uma porta à esquerda.");
+    criarSala(&sala_20, 20, "Voce esta na sala 20");
+    criarSala(&sala_8, 8, "voce esta na sala 8");
+
+    //aqui ira conectar as salas
+    conectarSalas(sala_inicial, sala_esquerda, sala_direita);
+    conectarSalas(sala_direita, sala_8, sala_20);
+
+    Sala* sala_atual = sala_inicial;
 
     int escolha;
-    
-    do{
-        limpaTela();
-        telaInicial();
-        scanf("%i",&escolha);
+    while (sala_atual != NULL) {
+        printf("Sala %i: %s\n", sala_atual->numero_sala, sala_atual->descricao);
 
-        switch (escolha)
-        {
-        case 1:
-            limpaTela();
-            printf("Meus livros.\n");
-            break;
+        if (sala_atual->esquerda != NULL && sala_atual->direita != NULL) {
+            printf("Escolha 1 para ir para a sala à esquerda e 2 para a sala à direita: ");
+            scanf("%i", &escolha);
 
-        case 2:
-            limpaTela();
-            printf("Inserir livro.\n");
-            break;
-
-        case 3:
-            limpaTela();
-            printf("Remover um livro.\n");
-            break;
-
-        case 4:
-            limpaTela();
-            printf("Informacoes gerais.\n");
-            break;
-
-        case 5:
-            exit(1);
-            
-        default:
-            limpaTela();
-            printf("\t\tOPCAO INCORRETA\n\n");
-        
+            if (escolha == 1) {
+                sala_atual = sala_atual->esquerda;
+            } else if (escolha == 2) {
+                sala_atual = sala_atual->direita;
+            } else {
+                printf("Escolha inválida. Tente novamente.\n");
+            }
+        } else {
+            sala_atual = NULL;
+            printf("Você chegou ao fim do labirinto.\n");
         }
-    }while(escolha < 1 || escolha > 5);
+    }
 
     return 0;
-}
-
-void telaInicial(){
-    printf("\t\tSIMULADOR DE BIBLIOTECA\n");
-    printf("Escolha alguma opcao: \n");
-    printf("1 - Meus livros.\n");
-    printf("2 - Inserir um livro.\n");
-    printf("3 - Remover um livro.\n");
-    printf("4 - Informacoes gerais.\n");
-    printf("5 - Sair.\n");
-
-}
-
-void limpaTela(){
-    #ifdef _WIN32
-    system("cls");
-    #else
-    system("clear");
-    #endif
 }
