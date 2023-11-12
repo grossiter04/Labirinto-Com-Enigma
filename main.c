c7220c0d6304f01934e21a14e940bb8d3a21536e
Gabriel Rossiter <gabrielrossiter04@hotmail.com>
2023-11-11 22:33:16 -0300

    adicionando dificuldade aos enigmas

--- main.c
+++ main.c
@@ -33,6 +33,10 @@ void conectarSalas(Sala* de, Sala* sala_esquerda, Sala* sala_direita) {
 
 void telaInicial(){
     printf("\t\tBEM VINDO\n");
+    printf("Escolha a dificuldade:\n");
+    printf("1 - Facil.\n");
+    printf("2 - Medio.\n");
+    printf("3 - Dificil.\n");
 }
 
 void liberarArvore(Sala* sala) {
@@ -95,20 +99,48 @@ int main() {
 
     telaInicial();
 
+    int dificuldade;
+
+    scanf("%i",&dificuldade);
+
     int topo = -1;
 
-    char listaResposta[15][15] = {"teste1", "teste2", "teste3", "teste4", "teste5", "teste6", "teste7", "teste8", "teste9", "teste10", "teste11", "teste12", "teste13", "teste14", "teste15"};
+    //em cima está as perguntas faceis e em baixo estão as respostas
+    char listaEnigmaFacil[15][100] = {"PerguntaFacil1", "PerguntaFacil2", "PerguntaFacil3", "PerguntaFacil4", "O que é aquela coisa que pode lá e pelo menos você vê?", "PerguntaFacil6", "PerguntaFacil7", "PerguntaFacil8", "PerguntaFacil9", "PerguntaFacil10", "PerguntaFacil11", "PerguntaFacil12", "PerguntaFacil13", "PerguntaFacil14", "PerguntaFacil15"};
+    char listaRespostaFacil[15][20] = {"RespostaFacil1", "RespostaFacil2", "RespostaFacil3", "RespostaFacil4", "escuridao", "RespostaFacil6", "RespostaFacil7", "RespostaFacil8", "RespostaFacil9", "RespostaFacil10", "RespostaFacil11", "RespostaFacil12", "RespostaFacil13", "RespostaFacil14", "RespostaFacil15"};
 
-    char palavra[15];
+    //em cima está as perguntas medias e em baixo estão as respontas
+    char listaEnigmaMedio[15][100] = {"PerguntasMedio1", "PerguntasMedio2", "PerguntasMedio3", "PerguntasMedio4", "PerguntasMedio5", "PerguntasMedio6", "PerguntasMedio7", "PerguntasMedio8", "PerguntasMedio9", "PerguntasMedio10", "PerguntasMedio11", "PerguntasMedio12", "PerguntasMedio13", "PerguntasMedio14", "PerguntasMedio15"};
+    char listaRespostaMedio[15][20] = {"RespostaMedio1", "RespostaMedio2", "RespostaMedio3", "RespostaMedio4", "RespostaMedio5", "RespostaMedio6", "RespostaMedio7", "RespostaMedio8", "RespostaMedio9", "RespostaMedio10", "RespostaMedio11", "RespostaMedio12", "RespostaMedio13", "RespostaMedio14", "RespostaMedio15"};
+
+    //em cima está as perguntas dificeis e em baixo estão as respostas
+    char listaEnigmaDificil[15][100] = {"PerguntaDificil1", "PerguntaDificil2", "PerguntaDificil3", "PerguntaDificil4", "PerguntaDificil5", "PerguntaDificil6", "PerguntaDificil7", "PerguntaDificil8", "PerguntaDificil9", "PerguntaDificil10", "PerguntaDificil11", "PerguntaDificil12", "PerguntaDificil13", "PerguntaDificil14", "PerguntaDificil15"};
+    char listaRespostaDificil[15][20] = {"RespostaDificil1", "RespostaDificil2", "RespostaDificil3", "RespostaDificil4", "RespostaDificil5", "RespostaDificil6", "RespostaDificil7", "RespostaDificil8", "RespostaDificil9", "RespostaDificil10", "RespostaDificil11", "RespostaDificil12", "RespostaDificil13", "RespostaDificil14", "RespostaDificil15"};
+
+
+    char palavra[20];
 
     int escolha;
     while (sala_atual != NULL) {
         printf("Sala %i: %s\n", sala_atual->numero_sala, sala_atual->descricao);
 
-        do{
-            printf("Resposta do enigma:\n");
-            scanf("%s",palavra);
-        }while(strcmp(palavra, listaResposta[sala_atual->numero_sala-1]) != 0);
+        if (dificuldade == 1){ //se o jogo for na dificuldade facil
+            do{
+                printf("%s\n",listaEnigmaFacil[sala_atual->numero_sala-1]);
+                scanf("%s", palavra);
+            }while(strcmp(palavra, listaRespostaFacil[sala_atual->numero_sala-1]) != 0);
+        } else if (dificuldade == 2){ //se o jogo for na dificuldade media
+            do{
+                printf("Resposta do enigma medio:\n");
+                scanf("%s", palavra);
+            }while(strcmp(palavra, listaRespostaMedio[sala_atual->numero_sala-1]) != 0);
+        } else if (dificuldade == 3){//se o jogo for na dificuldade dificil
+            do{
+                printf("Resposta do enigma Dificil:\n");
+                scanf("%s", palavra);
+            }while(strcmp(palavra, listaRespostaDificil[sala_atual->numero_sala-1]) != 0);
+        }
+
 
         if (sala_atual->esquerda != NULL || sala_atual->direita != NULL || sala_atual->esquerda == NULL || sala_atual->direita == NULL) {
             printf("Escolha 1 para ir para a sala a esquerda e 2 para a sala a direita, e 0 para voltar para sala anterior: ");
